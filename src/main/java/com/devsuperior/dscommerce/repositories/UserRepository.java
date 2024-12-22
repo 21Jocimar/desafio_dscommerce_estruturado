e74ec6c77d194ae42c5de6ec5b53332f9164af2e
Jocimar Oliveira <21jocimar@gmail.com>
2024-12-22 10:48:47 -0300

    GET user logged

--- src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
 
     @Query(nativeQuery = true, value = """
 			SELECT tb_user.email AS username, 
@@ -21,4 +21,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 			WHERE tb_user.email = :email
 		""")
     List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+
+	Optional<User> findByEmail(String email);
 }
